c001253d21ca6bd331211b4f7aefb9210c620430
Dima95 <Dima95@Dima95.com>
2015-02-16 16:55:41 +0200

    commit

---
+++ first/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace first
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(143, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 75);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Nulls";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Units";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(521, 312);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 399);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "cha";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ first/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace first
+{
+    public partial class Form1 : Form
+    {
+        Calculation c = new Calculation();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            chart1.ChartAreas[0].AxisX.Minimum = 0.5;
+            chart1.ChartAreas[0].AxisX.Maximum = 1.5;
+            chart1.ChartAreas[0].AxisX.IsLabelAutoFit = false;
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();            
+            if (textBox1.Text == "") return;
+            try
+            {
+                c = (Calculation)Int64.Parse(textBox1.Text);
+                label1.Text = c.ToString();
+                chart1.Series["Nulls"].Points.AddY(c.GetsNulls);
+                chart1.Series["Units"].Points.AddY(c.GetUnits);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Введіть, будь ласка, ціле число ", "Помилка");
+                textBox1.Text = "";
+                label1.Text = "";
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("Ввдено завелике число ", "Помилка");
+                textBox1.Text = "";
+                label1.Text = "";                
+            }
+        }
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    class Calculation
+    {
+        private Int64 numb;
+        private Int64 Units = 0,Nulls = 0;
+        Stack<Char> NumbChar = new Stack<Char>();
+        public Calculation() { }
+        private Calculation(Int64 i)
+        {
+            numb = i;
+            Convert();
+        }
+        public Int64 GetUnits
+        {
+            get { return Units; }
+        }
+        public Int64 GetsNulls
+        {
+            get { return Nulls; } 
+        }
+        void ToInt64(Int64 n)
+        {
+            numb = n;
+        }
+        public void Convert()
+        {
+            if (numb == 0)
+            {
+                NumbChar.Push('0');
+                return;
+            }
+            Int64 n = Math.Abs(numb); 
+            Int64 N;
+            while (n >= 1)
+            {
+                N = n % 2;
+                if (N == 0) { NumbChar.Push('0'); Nulls++; }
+                else { NumbChar.Push('1'); Units++; }
+                n /= 2; 
+            }
+        } 
+        public static explicit operator Calculation(Int64 n)
+        {
+            return new Calculation(n);
+        }
+        public override string ToString()
+        {
+            String s = null;   
+            foreach (Char x in NumbChar) s += x;
+            if (numb >= 0)
+                return s;
+            else return String.Format("{0}{1}","-",s); 
+        }
+    }
+}
